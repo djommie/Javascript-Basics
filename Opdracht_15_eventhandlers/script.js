7dd17928a01a2812c72c62269e5a9c764de4ff7b
djommie <64261030+djommie@users.noreply.github.com>
2021-05-06 15:38:25 +0200

    Add files via upload

--- Opdracht_15_eventhandlers/script.js
+++ Opdracht_15_eventhandlers/script.js
@@ -2,13 +2,11 @@ const btn = document.getElementById('mybutton')
 const btn2 = document.getElementById('mybutton2')
 const body = document.querySelector("body")
 
+
 const toggleColor = function () {
     body.classList.toggle('red-background'),
         body.classList.toggle('blue-background')
 }
-
-
-
 
 btn.addEventListener('click', function () {
     alert('button clicked')
